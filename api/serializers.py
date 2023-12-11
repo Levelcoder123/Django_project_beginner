2bc8c5e6aee4f500fc29be3621efb3cf739a0525
Levelcoder123 <muaz.abdulwahid@barq.dev>
2023-12-11 11:51:08 +0500

    create token for all type of users

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 
 from banks.models import Bank
+from users.models import User
+from accounts.models import Account
 
 
 class BanksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bank
-        fields = '__all__'
+        fields = '_all_'
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['number', 'type', 'amount']
